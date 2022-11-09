5ceb3c6eefdb08dc93fae1544eb28eab9b8c64bf
Dillan Kerai [sc21dmk] <sc21dmk@comp-pc1032.leeds.ac.uk>
2022-11-09 13:25:48 +0000

    find feature

---
+++ user/find.c
@@ -0,0 +1,44 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "kernel/fs.h"
+
+void find(char* one,char* two){
+	printf("one:%s; two:%s",one, two);
+}
+
+
+
+
+
+
+
+int main(int argc,char*argv[]){
+//	char buf[512], *p, *e, *b;
+//	int fd;
+//	struct stat st;
+//	struct direct de;
+	//printf("%s", argv[1]);
+	//Check if file can be opened 
+//	if ((fd=open(argv,0))<0){
+//		printf("File not found: %s\n", *argv);
+//	}
+
+	if (argc<3){
+		return -1;
+		exit(1);
+	}
+	find(argv[1],argv[2]);
+
+
+
+
+
+
+
+
+
+	exit(0);
+
+}
