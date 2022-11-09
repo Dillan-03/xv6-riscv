218a199f949a66fb81fe333a3d21fb70db78305a
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-11-09 21:24:47 +0000

    adding old code

--- user/find.c
+++ user/find.c
@@ -5,6 +5,31 @@
 #include "kernel/fs.h"
 
 void find(char* one,char* two){
+
+      char buf[512], *p, *e, *b;
+      int fd;
+      struct stat st;
+      struct direct de;
+       //Check if file can be opened 
+       if ((fd=open(argv,0))<0){
+               printf("File not found: %s\n", *argv);
+       }
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	printf("one:%s; two:%s",one, two);
 }
 
@@ -15,15 +40,6 @@ void find(char* one,char* two){
 
 
 int main(int argc,char*argv[]){
-//	char buf[512], *p, *e, *b;
-//	int fd;
-//	struct stat st;
-//	struct direct de;
-	//printf("%s", argv[1]);
-	//Check if file can be opened 
-//	if ((fd=open(argv,0))<0){
-//		printf("File not found: %s\n", *argv);
-//	}
 
 	if (argc<3){
 		return -1;
