8f9bd0ef3e70c7fa33f0e231ac441ef864225112
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-11-23 11:43:49 +0000

    header file updated (struct)

---
+++ user/memory_management.h
@@ -0,0 +1,15 @@
+//header file to hold the struct
+i
+//Meta information about the block
+struct block_data{
+	
+	//Size of block
+	size_t sizeBlock;
+	struct block *next;
+	int free;
+
+
+
+}block;
+
+
