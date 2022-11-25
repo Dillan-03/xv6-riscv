bbe0bde1fc604633b890f28d521d0fb7483e6fa9
Dillan Kerai [sc21dmk] <sc21dmk@comp-pc6138.leeds.ac.uk>
2022-11-25 12:56:52 +0000

    linked_list = 0

--- user/memory_management.c
+++ user/memory_management.c
@@ -2,10 +2,29 @@
 #include "memory_management.h"
 
 
+struct block_data* linked_head = 0;
+
 
 //malloc function
 void* _malloc(int size){
-	// void *value = sbrk(0);
+
+	// void *space = sbrk(4096);
+// 
+	//check for linked list being created
+	if (linked_head == 0){ //failed 
+
+		//create new block
+		linked_head = (struct block_data*) sbrk(4096);
+
+		linked_head->next = 0;
+		linked_head->free = 1;
+
+
+		linked_head->sizeBlock = PAGE_SIZE - sizeof( linked_head);
+		printf("%d\n",linked_head);
+		printf("created linked_head = 0");
+
+	}
 	// void *data = sbrk(size);
 	// if (data == (void*) - 1){
 	// 	return 0;
@@ -13,46 +32,46 @@ void* _malloc(int size){
 	// 	return value;
 	// }
 	
-	//Create struct
-	struct block_data* block_meta;
-	void *result = sbrk(0);
-	if (size<= 0){
-	//	printf("Malloc failed: unable to allocate memory");
-		return 0;
-	}
+// 	//Create struct
+// 	struct block_data* block_meta;
+// 	void *result = sbrk(0);
+// 	if (size<= 0){
+// 	//	printf("Malloc failed: unable to allocate memory");
+// 		return 0;
+// 	}
 
-	//No linked list
-	if (linked_head == 0){
-		block_meta = request_space(0,size); 
+// 	//No linked list
+// 	if (linked_head == 0){
+// 		block_meta = request_space(0,size); 
 	 
-		linked_head = block_meta;//create new linked list 
-	}
-	else{
-	//linked list is already created -> check for free memory
-		struct block_data *head = linked_head;
-		block_meta = available_block(&head, size);
+// 		linked_head = block_meta;//create new linked list 
+// 	}
+// 	else{
+// 	//linked list is already created -> check for free memory
+// 		struct block_data *head = linked_head;
+// 		block_meta = available_block(&head, size);
 
-		//unable to find available free blocks in linked list
-		if (block_meta == 0){
-			//printf("Unable to find available free block");
-			//request space -> sbrk 
-			block_meta = request_space(head, size);
-			if (!block_meta){
-				return 0;
-			}
-		}
-		//found free block to store data
-		else{
-			//initalise free as taken
-			//free = 0; memory is taken 
-			//free = 1: memory is avaiable
-			block_meta->free = 0;
+// 		//unable to find available free blocks in linked list
+// 		if (block_meta == 0){
+// 			//printf("Unable to find available free block");
+// 			//request space -> sbrk 
+// 			block_meta = request_space(head, size);
+// 			if (!block_meta){
+// 				return 0;
+// 			}
+// 		}
+// 		//found free block to store data
+// 		else{
+// 			//initalise free as taken
+// 			//free = 0; memory is taken 
+// 			//free = 1: memory is avaiable
+// 			block_meta->free = 0;
 
-		}
+// 		}
 		
-	}
-
-	return result+1;
+// 	}
+	// printf(space,"/n");
+	return linked_head;
 }
 
 
@@ -69,13 +88,14 @@ void _free(void *ptr){
 
 int main(){
 	printf("Testing Code: .......");
-	char *root = _malloc(5);
-	// _malloc(1);
-	// _malloc(2);
-	// _malloc(3);
-	for(int i =0; i < 26; i ++){
-		root[i] = i + 'A';
-	}
+	// char *root = _malloc(5);
+	int *test =_malloc(1);
+	printf("%d\n", test);
+	// // _malloc(2);
+	// // _malloc(3);
+	// for(int i =0; i < 26; i ++){
+	// 	root[i] = i + 'A';
+	// }
 
 	return 0;
 
--- user/memory_management.h
+++ user/memory_management.h
@@ -6,59 +6,66 @@
 
 // global variable head for our linked list
 
-void *linked_head = 0;
+// void *linked_head = 0;
+// struct block_data* linked_head;
+
+//Page 
+int PAGE_SIZE = 4096;
 
 //header file to hold the struct
 
 //Meta information about the block
-struct block_data{
+ struct block_data{
 	
 	//Size of block
 	int sizeBlock;
+	//Pointer to the next block
 	struct block_data *next;
+
+	//is data free
+	//free = 0 unavailable
 	int free;
 
 
 };
 
-//Parameter are the new created block, and the size of the space that is required to store the block
-struct block_data *request_space(struct block_data* tail, int size){
+// //Parameter are the new created block, and the size of the space that is required to store the block
+// struct block_data *request_space(struct block_data* tail, int size){
 
-        struct block_data *new_block;
+//         struct block_data *new_block;
 
-        //request space
-        new_block = (struct block_data*)sbrk(0);
+//         //request space
+//         new_block = (struct block_data*)sbrk(0);
 
-        //Check if memory can be allocated
+//         //Check if memory can be allocated
     
-        if ((void*)new_block == sbrk(size + sizeof(struct block_data))){
-			if (sbrk(size + sizeof(struct block_data)) == (void*) - 1){//failed
+//         if ((void*)new_block == sbrk(size + sizeof(struct block_data))){
+// 			if (sbrk(size + sizeof(struct block_data)) == (void*) - 1){//failed
 
-			//     printf("cannot request space from OS");
-					return 0; //sbrk failed
-			}
-		}
+// 			//     printf("cannot request space from OS");
+// 					return 0; //sbrk failed
+// 			}
+// 		}
 
 
-        if (tail == 0){ //linked list has not been created 
-                tail->next = new_block;
-        }
+//         if (tail == 0){ //linked list has not been created 
+//                 tail->next = new_block;
+//         }
+//         //Create block and assign meta data
+//         new_block->sizeBlock = size;
+//         new_block->next = 0;
+//         new_block->free = 0; //0 to initialise block has not been freed
+//         return new_block;
+// }
 
-        //Create block and assign meta data
-        new_block->sizeBlock = size;
-        new_block->next = 0;
-        new_block->free = 0; //0 to initialise block has not been freed
-        return new_block;
-}
+// //function which finds available space first through the linked list
+// struct block_data *available_block(struct block_data **data, int size){
+// 	//create new temp block that holds the head of the linked list
+// 	struct block_data *find = linked_head;
+// 	while (find && !(find->free == 0 && find->sizeBlock >= size)){
+// 		*data = find;
+// 		find = find->next;
+// 	}
+// 	return find; //return updated head of linked list 
 
-//function which finds available space first through the linked list
-struct block_data *available_block(struct block_data **data, int size){
-	//create new temp block that holds the head of the linked list
-	struct block_data *find = linked_head;
-	while (find && !(find->free == 0 && find->sizeBlock >= size)){
-		*data = find;
-		find = find->next;
-	}
-	return find; //return updated head of linked list 
-
-}
+// }
