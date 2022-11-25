44f86875750911f78f1c50931a0c3966e1d024c5
Dillan Kerai [sc21dmk] <sc21dmk@comp-pc6138.leeds.ac.uk>
2022-11-25 14:00:24 +0000

    progress ...

--- user/memory_management.c
+++ user/memory_management.c
@@ -1,33 +1,62 @@
 
 #include "memory_management.h"
 
-
 struct block_data* linked_head = 0;
+
+// struct block_data* linked_head = 0;
 
 
 //malloc function
 void* _malloc(int size){
 
 	// void *space = sbrk(4096);
-// 
+
 	//check for linked list being created
-	if (linked_head == 0){ //failed 
+	if (size <= 0){
+		return 0;
+	}else{
+		if (linked_head == 0){ //failed 
 
-		//create new block
-		linked_head = (struct block_data*) sbrk(4096);
+			//create new block
+			linked_head = (struct block_data*) sbrk(4096);
 
-		linked_head->next = 0;
-		linked_head->free = 1;
+			//NULL next block
+			linked_head->next = 0;
+			linked_head->free = 1;
 
 
-		linked_head->sizeBlock = PAGE_SIZE - sizeof( linked_head);
-		printf("%d\n",linked_head);
-		printf("created linked_head = 0");
+			linked_head->sizeBlock = PAGE_SIZE - sizeof(linked_head);
+			// printf("%d\n",linked_head);
+			printf("created linked_head = ");
 
-	}
-	// void *data = sbrk(size);
-	// if (data == (void*) - 1){
-	// 	return 0;
+		}
+		else{
+			//Pool of memory is already created. 
+			//We don't which block is taken or free
+			struct block_data *block = linked_head;
+
+			//find available blocks in heap 
+			block = available_block(&block, size); 
+			
+			//available space is not found
+			//request space from OS
+			if (!block){
+				block = request_space(block,4096);
+				// linked_head ->next = block;
+				printf("free block available at loc: ");
+				// return linked_head->next;
+			}else{
+				// linked_head->next = block;
+				block->free = 0 ;
+				block->sizeBlock = PAGE_SIZE - sizeof(block); 
+			}
+
+			// linked_head->free = 0;
+			
+			// block->next = 0;
+			// block->free = 1;
+			// block->sizeBlock = PAGE_SIZE - sizeof(block);
+			//assign pointer to next blocksize
 	// } else{
 	// 	return value;
 	// }
@@ -69,9 +98,10 @@ void* _malloc(int size){
 
 // 		}
 		
-// 	}
+	}
+	return linked_head+1;
 	// printf(space,"/n");
-	return linked_head;
+	// return linked_head;
 }
 
 
@@ -88,10 +118,22 @@ void _free(void *ptr){
 
 int main(){
 	printf("Testing Code: .......");
+	printf("\n");
+
+
 	// char *root = _malloc(5);
 	int *test =_malloc(1);
-	printf("%d\n", test);
-	// // _malloc(2);
+	printf("%d \n", test);
+
+	int *testtwo =_malloc(1);
+	printf("%d \n", testtwo);
+
+	int *testthree =_malloc(1);
+	printf("%d \n", testthree);
+
+	// printf("%d, %d\n", test, testtwo);
+
+	// _malloc(2);
 	// // _malloc(3);
 	// for(int i =0; i < 26; i ++){
 	// 	root[i] = i + 'A';
--- user/memory_management.h
+++ user/memory_management.h
@@ -4,6 +4,9 @@
 #include "user/user.h"
 
 
+
+// struct block_data* linked_head = 0;
+
 // global variable head for our linked list
 
 // void *linked_head = 0;
@@ -29,43 +32,45 @@ int PAGE_SIZE = 4096;
 
 };
 
-// //Parameter are the new created block, and the size of the space that is required to store the block
-// struct block_data *request_space(struct block_data* tail, int size){
+//Parameter are the new created block, and the size of the space that is required to store the block
+struct block_data *request_space(struct block_data* tail, int size){
 
-//         struct block_data *new_block;
+        //request space
+		struct block_data *new_block;
+        new_block = (struct block_data*)sbrk(size);
 
-//         //request space
-//         new_block = (struct block_data*)sbrk(0);
-
-//         //Check if memory can be allocated
+        //Check if memory can be allocated
     
-//         if ((void*)new_block == sbrk(size + sizeof(struct block_data))){
-// 			if (sbrk(size + sizeof(struct block_data)) == (void*) - 1){//failed
+        if ((void*)new_block == sbrk(size + sizeof(struct block_data))){
+			if (sbrk(size + sizeof(struct block_data)) == (void*) - 1){//failed
 
-// 			//     printf("cannot request space from OS");
-// 					return 0; //sbrk failed
-// 			}
-// 		}
+			//     printf("cannot request space from OS");
+					return 0; //sbrk failed
+			}
+		}
 
+		// if (tail == 0){
+			// tail->next = new_block;
+		// }
+        
+        //Create block and assign meta data
+        new_block->sizeBlock = PAGE_SIZE - sizeof(new_block);
+        new_block->next = 0;
+        new_block->free = 0; //0 to initialise block has not been freed
+		// linked_head-> free = 1;
+		// linked_head -> next = new_block;
 
-//         if (tail == 0){ //linked list has not been created 
-//                 tail->next = new_block;
-//         }
-//         //Create block and assign meta data
-//         new_block->sizeBlock = size;
-//         new_block->next = 0;
-//         new_block->free = 0; //0 to initialise block has not been freed
-//         return new_block;
-// }
+        return new_block;
+}
 
 // //function which finds available space first through the linked list
-// struct block_data *available_block(struct block_data **data, int size){
-// 	//create new temp block that holds the head of the linked list
-// 	struct block_data *find = linked_head;
-// 	while (find && !(find->free == 0 && find->sizeBlock >= size)){
-// 		*data = find;
-// 		find = find->next;
-// 	}
-// 	return find; //return updated head of linked list 
+struct block_data *available_block(struct block_data **data, int size){
+	//create new temp block that holds the head of the linked list
+	struct block_data *find = *data;
+	while (find && !(find->free == 0 && find->sizeBlock >= size)){
+		*data = find;
+		find = find->next;
+	}
+	return find; //return updated head of linked list 
 
-// }
+}
